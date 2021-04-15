171defecda4f05adf9554d54b3c128b6cc5c8b85
AMITH-PC\Amith <amithy25@gmail.com>
2021-04-15 15:50:19 -0400

    added match api backend

--- src/main/java/io/amith/ipldashboard/controller/TeamController.java
+++ src/main/java/io/amith/ipldashboard/controller/TeamController.java
@@ -1,5 +1,6 @@
 package io.amith.ipldashboard.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
@@ -7,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.amith.ipldashboard.model.Match;
@@ -28,17 +30,23 @@ public class TeamController {
   
   @GetMapping("/team/{teamName}")
   public Team getTeam(@PathVariable String teamName){
-
     Team team =  teamRepository.findByTeamName(teamName);
-    
     team.setMatches( matchRepository.findLatestMatches(teamName, 4));
-
-
     return team;
-
   }
 
-
+  @GetMapping("/team/{teamName}/matches")
+  public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+    
+    LocalDate startDate = LocalDate.of(year, 1, 1);
+    LocalDate endDate = LocalDate.of(year+1, 1, 1);
+    return matchRepository.getMatchesByTeamBetweenDates(
+                    teamName, 
+                    startDate, 
+                    endDate
+                    );
+    
+  }
 
   
 
