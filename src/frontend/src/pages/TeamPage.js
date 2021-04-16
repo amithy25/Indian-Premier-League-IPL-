f6b5189cdc8e82bc00a4d348e1a0bd222fcce6d7
AMITH-PC\Amith <amithy25@gmail.com>
2021-04-16 14:39:57 -0400

    added home page and additional styles

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { PieChart } from 'react-minimal-pie-chart';
@@ -12,18 +12,23 @@ export const TeamPage = () => {
   const { teamName } = useParams();
 
   useEffect(() => {
-    const fetchMatches = async () => {
+    const fetchTeam = async () => {
       const response = await fetch(`http://localhost:8080/team/${teamName}`);
       const data = await response.json();
       setTeam(data);
     };
-    fetchMatches();
+    fetchTeam();
   }, [teamName]);
 
   if (!team || !team.teamName) return <h1>Team Not Found!</h1>;
 
   return (
     <div className="TeamPage">
+      <div>
+        <h2>
+          <Link to="/team">All Teams</Link>
+        </h2>
+      </div>
       <div className="team-name-section">
         <h1 className="team-name">{team.teamName}</h1>
       </div>
@@ -52,7 +57,11 @@ export const TeamPage = () => {
       ))}
 
       <div className="more-link">
-        <a href="#">More > </a>
+        <Link
+          to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}
+        >
+          More >
+        </Link>
       </div>
     </div>
   );
--- src/main/java/io/amith/ipldashboard/controller/TeamController.java
+++ src/main/java/io/amith/ipldashboard/controller/TeamController.java
@@ -48,6 +48,9 @@ public class TeamController {
     
   }
 
-  
+  @GetMapping("/team")
+  public Iterable <Team> getAllTeams() {
+    return teamRepository.findAll();
+  }
 
 }
